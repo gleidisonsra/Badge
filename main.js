8cbaafc9a89fc9ec5cb4fb364f572370181d2e4b
Gleidison Santa Rosa Almeida <71789654+gleidisonsra@users.noreply.github.com>
2021-11-02 17:54:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+      const linksSocialMedia = {
+        github : 'gleidisonsra',
+        youtube :'UC693jlEP6qS6tCa3A6ES5ZQ',
+        facebook :'gleidisonsr',
+        instagram : 'sragleidison',
+        twitter :'@gleidisonsra'
+     }
+     function changeSocialMediaLinks() {    
+      for (let li of socialLinks.children){
+        const social = li.getAttribute("class")
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`        
+      
+      }
+     }
+     
+     changeSocialMediaLinks()
+
+     function getGitHubProfileInfos(){
+       const url =`https://api.github.com/users/${linksSocialMedia.github}`
+       fetch(url)
+       .then(response => response.json())
+       .then(data => {
+         userName.textContent = data.name
+         userBio.textContent = data.bio
+         userLink.href = data.html_url
+         userImage.src = data.avatar_url
+         userLogin.textContent = data.login
+
+       })
+     }
+     getGitHubProfileInfos()
+
+    
+
+
+     
+
+
